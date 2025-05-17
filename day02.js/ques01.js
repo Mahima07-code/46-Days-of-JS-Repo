380396526f126feb0c01708844ede112fc4380b8
Mahma Prajapati <mahimaprajapati85@outlook.com>
2025-05-17 15:27:49 +0530

    Day02

---
+++ day02.js/ques01.js
@@ -0,0 +1,9 @@
+// 💥 Q4. Can you add a number and a string in JS?
+let a = 5+ "5"
+console.log(a); // ??
+console.log(typeof a)
+
+// A.
+// Oh, JS be like: “Math? Nah, let’s do Text!”
+// 🧠 Output: "55"
+// Because numbers + strings = string party 🎉
---
+++ day02.js/ques02.js
@@ -0,0 +1,7 @@
+// 👶 Q5. What is typeof undefined and typeof null?
+// A.
+
+console.log(typeof undefined) // "undefined" 
+console.log(typeof null)      // "object" 
+
+// Because JS was born in 10 days and didn’t have time for logic 😅
