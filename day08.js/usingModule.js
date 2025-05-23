a07baa5884ee509d75758369d6bbb3794e094863
Mahma Prajapati <mahimaprajapati85@outlook.com>
2025-05-23 20:40:49 +0530

    Update

--- day08.js/usingModule.js
+++ day08.js/usingModule.js
@@ -4,5 +4,5 @@ import { greet } from './modules.js';
 console.log(greet("Mahima"));
 
 import { add } from './modules.js';
-console.log(`The sum of given no is ${add(2, 3)}`); 
+console.log(add(2, 3)); 
 
