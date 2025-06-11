ab5f2923aee7c54c00879f53b2f97a42cc34a2b8
Mahima <mahimarn84@gmail.com>
2025-06-11 22:32:44 +0530

    Day27

---
+++ Day27/file1.js
@@ -0,0 +1,22 @@
+// -- Array --
+
+let fruits = ['apple', 'banana', 'cherry'];
+console.log(fruits[1]); // banana
+
+// -- Object -- 
+
+let person = { name: 'John', age: 25 };
+console.log(person.name); // John
+
+// -- Map -- 
+
+let map = new Map();
+map.set('name', 'Alice');
+map.set(1, 'One');
+console.log(map.get(1)); // One
+
+// -- Sets --
+
+let numbers = new Set([1, 2, 2, 3]);
+console.log(numbers); // Set(3) {1, 2, 3}
+
